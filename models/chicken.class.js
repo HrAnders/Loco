da5cdf16ead99872abab8734d1af3d942eab6285
Stefan Anders <stefananders@hotmail.de>
2023-06-16 22:27:29 +0200

    added cloud movement

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -7,6 +7,22 @@ class Chicken extends MovableObject{
     constructor(){
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.x = 200 + Math.random()*400;
+        this.animate();
         
     }
+
+    animate(){
+        setInterval(()=>{
+            this.x-=1;
+            this.loadImage('img/3_enemies_chicken/chicken_normal/1_walk/2_w.png');
+        },1000/60);
+
+        setInterval(()=>{
+            this.x-=1;
+            this.loadImage('img/3_enemies_chicken/chicken_normal/1_walk/3_w.png');
+        },1000/60);
+
+    }
+
+
 }
--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,11 +1,20 @@
 class Cloud extends MovableObject{
     height = 200;
     width = 400;
+    ctx = canvas.getContext("2d");
+    
 
     constructor(){
         super().loadImage('img/5_background/layers/4_clouds/1.png');
-        this.x = Math.random()*400;
+        this.x = Math.random() * 500;
+        this.animate();
         this.y = 50
+    }
+
+    animate(){
+        setInterval(()=>{
+        this.x -= 0.2;
+        },1000/60) //entspricht 60fps
     }
 
 }
