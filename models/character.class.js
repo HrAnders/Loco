376513424cfa294eb080fcf30439a5f1707556ee
Stefan Anders <stefananders@hotmail.de>
2023-06-19 21:42:54 +0200

    added imageCache

--- models/character.class.js
+++ models/character.class.js
@@ -2,12 +2,34 @@ class Character extends MovableObject{
     width = 100;
     height = 250;
 
+    IMAGES_WALKING = [
+        '../img/2_character_pepe/2_walk/W-21.png',
+        '../img/2_character_pepe/2_walk/W-22.png',
+        '../img/2_character_pepe/2_walk/W-23.png',
+        '../img/2_character_pepe/2_walk/W-24.png',
+        '../img/2_character_pepe/2_walk/W-25.png',
+        '../img/2_character_pepe/2_walk/W-26.png',
+    ]
+
+    currentImage = 0;
+
     constructor(){
         super().loadImage('../img/2_character_pepe/1_idle/idle/I-1.png');
-        
+        this.loadImages(this.IMAGES_WALKING)
+
+        this.animate();
     }
 
     jump(){
 
+    }
+
+    animate(){
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length; //modulo-fkt berechnet rest: z.b. 1 / 6 --> ergebnis 0, rest 1
+            let path = this.IMAGES_WALKING[i]; // das i entspricht dem modulo-rest; es kann nicht außerhalb der length liegen
+            this.img = this.imageCache[path];
+            this.currentImage ++; 
+        }, 100);
     }
 }
--- models/movableObject.class.js
+++ models/movableObject.class.js
@@ -4,10 +4,25 @@ class MovableObject{
     img;
     height = 75;
     width = 150;
+    imageCache = {};
 
     loadImage(path){
         this.img = new Image(); // entspricht dem img-tag in html --> this.img = document.getElementById('image) --> <img id="image">
         this.img.src = path;
+    }
+
+    /**
+     * 
+     * @param {Array} arr - array with image paths
+     */
+    loadImages(arr){  //der funktion wird ein array mit img-pfaden übergeben
+        arr.forEach((path) => {     //aus jedem pfad des arrays wird ein image-objekt gemacht, indem die source des neuen img-objekts aus dem arraypfad gespeist wird
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;  //dem imagecache-array wird beim key "path" der value "img" zugefügt --> so stehen alle img-objekte dann im image-cache
+        })
+        
+
     }
 
     moveRight(){
